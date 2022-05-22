991cb52f4412b2e0c88f2ac9b09ebdd6f7c6f895
Artur Mroczkowski <a.mroczkowski@intive.com>
2022-05-22 21:14:27 +0200

    Init

---
+++ src/test/java/pl/altkom/springcloud/lab01/resttemplate/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.springcloud.lab01.resttemplate.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
